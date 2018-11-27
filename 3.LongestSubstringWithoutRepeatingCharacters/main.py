c4541f3e725941de62ca7491a17237fd00ec4ca5
Lionel Shen <lionel.shen@autodesk.com>
2018-11-27 16:02:21 +0800

    add third problem

---
+++ 3.LongestSubstringWithoutRepeatingCharacters/main.py
@@ -0,0 +1,17 @@
+class Solution:
+
+  """
+  :type s: str
+  :rtype: int
+  """
+  def lengthOfLongestSubstring(self, s):
+    pass
+
+
+def main():
+  sln = Solution()
+  result = sln.lengthOfLongestSubstring("")
+  print(result)
+
+if __name__ == "__main__":
+  main()
